a6c8b0a642ab3eb82e0b0b3c671e610752f9628f
ragnarp12 <ragnarp12@ru.is>
2014-12-03 12:00:39 +0000

    Nothing

--- runDocker.sh
+++ runDocker.sh
@@ -4,5 +4,5 @@ set -e
 
 PRODUCTION_SERVER="188.226.210.128"
 
-echo Connect to Production server and run docker image
+echo Connecting to Production server and run docker image
 ssh -o ConnectTimeout=10 root@$PRODUCTION_SERVER 'bash -s' < dockerserver.sh

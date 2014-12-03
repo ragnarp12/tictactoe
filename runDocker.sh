e8170192cef69a4cdba05efe07dec75f6ccb7b4c
ragnarp12 <ragnarp12@ru.is>
2014-12-03 09:36:30 +0000

    Added ssh timeout to server

--- runDocker.sh
+++ runDocker.sh
@@ -5,4 +5,4 @@ set -e
 PRODUCTION_SERVER="188.226.210.128"
 
 echo Connect to Production server and run docker image
-ssh root@$PRODUCTION_SERVER 'bash -s' < dockerserver.sh
+ssh -o ConnectTimeout=10 root@$PRODUCTION_SERVER 'bash -s' < dockerserver.sh

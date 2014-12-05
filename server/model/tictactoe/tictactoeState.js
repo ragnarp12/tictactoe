8dc20dfd591f81094fb49210ac9f1a1821f71cbf
ragnarp12 <ragnarp12@ru.is>
2014-12-05 14:33:05 +0000

    Added isOutofBounds check, switch player, draw for debugging and resetBoard also for debug

--- server/model/tictactoe/tictactoeState.js
+++ server/model/tictactoe/tictactoeState.js
@@ -2,10 +2,13 @@ var _ = require('lodash');
 
 module.exports = function(history) {
 
+    var row;
+    var col;
+    var player1, player2;
     var gameFull = false;
     var gameCreated = false;
-    var currentPlayer;
-
+    var currentPlayer = null;
+    var gridSize = 3;
     var gameBoard = [
         ['', '', ''],
         ['', '', ''],
@@ -14,27 +17,55 @@ module.exports = function(history) {
 
     _.each(history, function(event) {
         if (event.event === "GameJoined") {
+            player2 = event.user;
             gameFull = true;
         }
         if (event.event === "GameCreated") {
             currentPlayer = event.user;
+            player1 = event.user;
             gameCreated = true;
+        }
+        if(event.event === "") {
+          
         }
     });
 
-    function isFree(coord) {
-        var col = coord[0];
-        var row = coord[1];
+    function isFree() {
         return (gameBoard[col][row] === '');
     }
 
-    function movePlayer(coord) {
-        if (isFree(coord)) {
+    function isOutOfBounds() {
+        return ((row >= gridSize) || (col >= gridSize));
+    }
+
+    function switchPlayer() {
+        currentPlayer = (currentPlayer.username === player1.username ? player2.username : player1.username);
+    }
+
+    function movePlayer() {
+        if (isFree() && !isOutOfBounds()) {
+            gameBoard[row][col] = (currentPlayer.username === player1.username ? 'X' : 'O');
+            switchPlayer();
+            resetBoard();
+            draw();
             return true;
         }
         return false;
     }
 
+    function resetBoard () {
+      for(i = 0; i<gridSize; i++) {
+        gameBoard[i][0] = (gameBoard[i][0] === '' ? '-' : gameBoard[i][0]);
+        gameBoard[i][1] = (gameBoard[i][1] === '' ? '-' : gameBoard[i][1]);
+        gameBoard[i][2] = (gameBoard[i][2] === '' ? '-' : gameBoard[i][2]);
+      }
+    }
+
+    function draw() {
+      for(i = 0; i<gridSize; i++) {
+        console.log(gameBoard[i][0] + gameBoard[i][1] + gameBoard[i][2] );
+      }
+    }
     return {
         gameFull: function() {
             return gameFull;
@@ -46,7 +77,9 @@ module.exports = function(history) {
             return gameCreated;
         },
         movePlayer: function(coord) {
-            return movePlayer(coord);
+            row = coord[0];
+            col = coord[1];
+            return movePlayer();
         }
 
     }

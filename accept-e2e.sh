2da9853a3a4a043b0638a1447cb53a90f9ad4cee
ragnarp12 <ragnarp12@ru.is>
2014-12-19 04:08:33 +0000

    Changed e2e script

--- accept-e2e.sh
+++ accept-e2e.sh
@@ -3,14 +3,14 @@
 export PATH=$PATH:/usr/local/bin
 export DISPLAY=:99
 
-echo "Check if xvfb-run is installed"
-if ! which xvfb-run > /dev/null; then
-	echo "Installing"
-	apt-get install xvfb
-	echo "Installed"
-else
-	echo "Xvfb is installed"
-fi
+#echo "Check if xvfb-run is installed"
+#if ! which xvfb-run > /dev/null; then
+#	echo "Installing"
+#	apt-get install xvfb
+#	echo "Installed"
+#else
+#	echo "Xvfb is installed"
+#fi
 
 echo "Cleaning..."
 rm -rf ./dist
@@ -18,9 +18,9 @@ rm -rf ./dist
 echo "Install Bower components"
 bower install --no-color
 rc=$?
-if [[ $rc != 0 ]] ; then
-echo "Bower failed to install components " $rc
-exit $rc
+if [ $rc != 0 ]; then
+	echo "Bower failed to install components " $rc
+	exit $rc
 fi
 
 
@@ -28,9 +28,9 @@ echo "Install NPM packages"
 npm install --no-color
 
 rc=$?
-if [[ $rc != 0 ]] ; then
-echo "NPM failed to install components " $rc
-exit $rc
+if [ $rc != 0 ]; then
+	echo "NPM failed to install components " $rc
+	exit $rc
 fi
 
 echo "Run web driver update"
@@ -41,9 +41,9 @@ node node_modules/grunt-protractor-runner/node_modules/protractor/bin/webdriver-
 echo "Run E2E tests"
 xvfb-run grunt test:e2e
 rc=$?
-if [[ $rc != 0 ]] ; then
-echo "Grunt E2E tests failed " $rc
-exit $rc
+if [ $rc != 0 ]; then
+	echo "Grunt E2E tests failed " $rc
+	exit $rc
 fi
 
 killall Xvfb
